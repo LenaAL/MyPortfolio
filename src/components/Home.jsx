84a8970f945d7231c20c431606649904ad76e8a5
LenaAL <lenaamdallarsen@gmail.com>
2022-11-23 12:23:53 +0100

    FSDK,SVD

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
                     <h2 className="undertext">Junior Web Designer </h2>
                 </div>
                 <div className="buttonContainer">
-                    <NavLink to="/project"> <button className="projectBtn"><span>View my work</span></button> </NavLink>
+                    <NavLink to="/project"> <button className="projectBtn"><span>View My Work</span></button> </NavLink>
                     <NavLink to="/post"> <button className="reflectBtn"><span>My Reflections</span></button> </NavLink>
                     <NavLink to="/about"> <button className="aboutBtn"><span>About Me</span></button> </NavLink>
                 </div>
