42da992f7e3ba547ac85cf6592cc1a8f067903c0
LenaAL <lenaamdallarsen@gmail.com>
2022-11-23 11:48:05 +0100

    chnaged bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import image from '../img/alexander-grey-62vi3TG5EDg-unsplash.jpg';
+import image from '../img/dmytro-tolokonov-Jq3WI9IQgEs-unsplash (1).jpg';
 
 
 
